b042dbafed55b6c426a95b1d88c03e4bf1dd5b57
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-12-27 17:41:52 -0800

    Just solved the airport robot problem.
    Learned to get used to defining empty structs with no fields. I also learned how to make structs conform to certain interfaces by implementing the methods required by those itnerfaces. Did not get to use empty interfaces for Generic programming, but I will eventually get there.

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,38 @@
 package airportrobot
 
+import "fmt"
+
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
+
+type Greeter interface {
+	// name of language to greet the visitor in
+	LanguageName() string
+	// greets the visitor saying "Hallo {name}!"
+	Greet(name string) string
+}
+
+type Italian struct{}
+
+type Portuguese struct{}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
+
+func SayHello(name string, g Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", g.LanguageName(), g.Greet(name))
+}
