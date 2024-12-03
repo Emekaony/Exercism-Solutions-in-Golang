6d1663b10050957d7713d0f471b24857ef1a449d
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-12-03 00:46:18 -0800

    Solved raindrops exercise

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,5 +1,25 @@
 package raindrops
 
+import (
+	"fmt"
+	"strings"
+)
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	// builder is a fast way manipulate strings in a mutable way
+	builder := strings.Builder{}
+	if number%3 == 0 {
+		builder.WriteString("Pling")
+	}
+	if number%5 == 0 {
+		builder.WriteString("Plang")
+	}
+	if number%7 == 0 {
+		builder.WriteString("Plong")
+	}
+	if number%7 != 0 && number%3 != 0 && number%5 != 0 {
+		// does this work??
+		builder.WriteString(fmt.Sprint(number))
+	}
+	return builder.String()
 }
