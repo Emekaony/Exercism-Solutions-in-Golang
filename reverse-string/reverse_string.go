df90cb34fc87775689ada4be792d572edfd6d1f4
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-08-12 02:05:07 -0700

    useful comment

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -10,6 +10,7 @@ func Reverse(input string) string {
 	*/
 	r := []rune(input)
 	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		// go lets u swap like this without a temp value
 		r[i], r[j] = r[j], r[i]
 	}
 	return string(r)
