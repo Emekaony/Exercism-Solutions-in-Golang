ceaf050df929342fd0aa52ef20fdb3ac829350fc
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-08-11 21:06:32 -0700

    Easy ahh problem

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,20 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, errors.New("number must be greater than 1")
+	}
+
+	count := 0
+	for n > 1 {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+		count++
+	}
+	return count, nil
 }
